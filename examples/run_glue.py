18ab9cd0e84f5c92a35cff359e79955ff5b45493
shreeraman96 <shreeraman2013@gmail.com>
2020-03-12 18:16:51 -0400

    debug for run_glue.py

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -200,18 +200,21 @@ def train(args, train_dataset, model, tokenizer):
         epochs_trained, int(args.num_train_epochs), desc="Epoch", disable=args.local_rank not in [-1, 0],
     )
     set_seed(args)  # Added here for reproductibility
-    print("------------------------------debug 2 ---------------------------------------")
     for _ in train_iterator:
         epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
         for step, batch in enumerate(epoch_iterator):
 
             # Skip past any already trained steps if resuming training
             if steps_trained_in_current_epoch > 0:
+                print("------------------------------debug 2 ---------------------------------------")
                 steps_trained_in_current_epoch -= 1
                 continue
 
             model.train()
+            print("------------------------------debug 3 ---------------------------------------")
             batch = tuple(t.to(args.device) for t in batch)
+
+            print("------------------------------debug 4 ---------------------------------------")
             inputs = {"input_ids": batch[0], "attention_mask": batch[1], "labels": batch[3]}
             if args.model_type != "distilbert":
                 inputs["token_type_ids"] = (
